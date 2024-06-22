df9fbc20b3352acf11f79e60392f861f3eec7e38
UmenoKoudai <umenokoudai0805@gmail.com>
2024-06-22 18:20:15 +0900

    初コミット

---
+++ BackGround.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+namespace constnts {
+	namespace BackGround {
+		const Size BLOCK_SIZE{ 50, 50 };
+		const int MAPSIZE_X = 100;
+		const int MAPSIZE_Y = 100;
+		const int MAX = MAPSIZE_X * MAPSIZE_Y;
+	}
+}
+
+class BackGround {
+	Rect map[constnts::BackGround::MAX];
+
+	BackGround() {
+	}
+};
